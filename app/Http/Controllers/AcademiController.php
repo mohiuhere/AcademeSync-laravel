3634576ccc3f4ba7bb89f4a57e3faf136900fff8
mohiuhere <mohiuhere@gmail.com>
2023-12-19 15:55:29 +0600

    add migration files and font-end files

---
+++ app/Http/Controllers/AcademiController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AcademiController extends Controller
+{
+    public function addClassIndex(){
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AcademiController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.pages.admin-dashbord');
 });
+
+
+Route::get('/add/class', [AcademiController::class, 'addClassIndex']);
