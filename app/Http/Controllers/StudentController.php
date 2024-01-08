6571f0c3c1423268f5834b596245fbd9b8b84107
mohiuhere <mohiuhere@gmail.com>
2024-01-08 17:11:08 +0600

    student not done

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use App\Models\Religion;
 use App\Models\Gender;
 use App\Models\BloodGroup;
 use App\Models\Classes;
 use App\Models\Section;
+use App\Models\User;
+use App\Models\Student;
 
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     public function studentIndex(){
-        return view('admin.pages.students.student');
+        $students = DB::select(
+            "SELECT students.id as id,
+            students.roll as roll_no,
+            users.first_name as first_name,
+            users.last_name as last_name,
+            users.mobile as mobile,
+            users.email as email,
+            students.admission_date as admission_date,
+            students.status as status
+            FROM users JOIN students ON users.id = students.user_id;"
+        );
+        
+        return view('admin.pages.students.student', [
+            'students' => $students
+        ]);
     }
 
     public function createStudentIndex(){
@@ -50,7 +67,57 @@ class StudentController extends Controller
             'status' => 'required|boolean',
             'address' => 'required|string|max:255',
         ]);
-        dd($validated);
+        if(request()->has('image_url')){
+            $image_path = request()->file('image_url')->store('user-profile', 'public');
+            $validate['image'] = $image_path;
+
+            $user = new User;
+            $user->first_name = $req->first_name;
+            $user->last_name = $req->last_name;
+            $user->user_type = 'student';
+            $user->mobile = $req->mobile;
+            $user->email = $req->email;
+            $user->password = '123456';
+            $user->image_url = $image_path;
+            $user->date_of_birth = $req->date_of_birth;
+            $user->blood_group_id = $req->blood_group_id;
+            $user->gender_id = $req->gender_id;
+            $user->religion_id = $req->religion_id;
+            $user->address = $req->address;
+            $user->save();
+
+            $student = new Student;
+            $student->admission_id = $req->admission_no;
+            $student->roll = $req->roll_no;
+            $student->user_id = $user->id;
+            $student->admission_date = $req->admission_date;
+            $student->status = $req->status;
+            $student->save();
+        }else{
+            $user = new User;
+            $user->first_name = $req->first_name;
+            $user->last_name = $req->last_name;
+            $user->user_type = 'student';
+            $user->mobile = $req->mobile;
+            $user->email = $req->email;
+            $user->password = '123456';
+            $user->image_url = 'public\img\undraw_profile_3.svg';
+            $user->date_of_birth = $req->date_of_birth;
+            $user->blood_group_id = $req->blood_group_id;
+            $user->gender_id = $req->gender_id;
+            $user->religion_id = $req->religion_id;
+            $user->address = $req->address;
+            $user->save();
+
+            $student = new Student;
+            $student->admission_id = $req->admission_no;
+            $student->roll = $req->roll_no;
+            $student->user_id = $user->id;
+            $student->admission_date = $req->admission_date;
+            $student->status = $req->status;
+            $student->save();
+        }
+        return redirect()->route('student.index');
     }
 
     public function promoteStudentIndex(){
--- resources/views/admin/pages/students/student.blade.php
+++ resources/views/admin/pages/students/student.blade.php
@@ -19,21 +19,30 @@
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                 <thead>
                     <tr>
+                        <th>Student Roll</th>
                         <th>Name</th>
+                        <th>Mobile</th>
+                        <th>Email</th>
+                        <th>Admission Date</th>
                         <th>Status</th>
                         <th>Action</th>
                     </tr>
                 </thead>
                 <tfoot>
                     <tr>
+                        <th>Student Roll</th>
                         <th>Name</th>
+                        <th>Mobile</th>
+                        <th>Email</th>
+                        <th>Admission Date</th>
                         <th>Status</th>
                         <th>Action</th>
                     </tr>
                 </tfoot>
                 <tbody>
+                    @foreach ($students as $student)
                     <tr>
-                        <td>Mid</td>
+                        <td>{{ $student->roll_no }}</td>
                         <td>Active</td>
                         <td>
                             <div class="dropdown">
@@ -47,21 +56,7 @@
                             </div>
                         </td>
                     </tr>
-                    <tr>
-                        <td>Final</td>
-                        <td>Active</td>
-                        <td>
-                            <div class="dropdown">
-                                <button class="btn btn-primary btn-dropdown" type="button" data-toggle="dropdown" aria-expanded="false">
-                                    <i class="fa-solid fa-list"></i>
-                                </button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="#">Edit</a>
-                                    <a class="dropdown-item" href="#">Delete</a>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
